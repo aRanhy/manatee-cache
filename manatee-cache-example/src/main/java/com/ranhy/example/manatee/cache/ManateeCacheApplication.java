e7d09ca9d7bee6bbe6487ae86c64174b4e9031bb
aRanhy <476650898@qq.com>
2021-03-17 23:37:17 +0800

    add example

--- manatee-cache-example/src/main/java/com/ranhy/example/manatee/cache/ManateeCacheApplication.java
+++ manatee-cache-example/src/main/java/com/ranhy/example/manatee/cache/ManateeCacheApplication.java
@@ -1,9 +1,11 @@
 package com.ranhy.example.manatee.cache;
 
+import com.ranhy.framework.manatee.cache.client.annotation.EnableManateeCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableManateeCache
 public class ManateeCacheApplication {
 
 	public static void main(String[] args) {
